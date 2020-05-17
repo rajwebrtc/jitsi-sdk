fe67a894af43d4d79ccc69694a5ae795ff5ac613
raj yadav <raj.webrtc@gmail.com>
2020-05-17 21:10:44 +0530

    android sdk

---
+++ app/build/generated/source/buildConfig/release/org/jitsi/meet/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.jitsi.meet;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "org.jitsi.meet";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4333972;
+  public static final String VERSION_NAME = "20.2.0";
+  // Fields from build type: release
+  public static final boolean GOOGLE_SERVICES_ENABLED = false;
+  public static final boolean LIBRE_BUILD = false;
+}
